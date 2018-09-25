d7e23a88a8ab023712a13a861bad111c981ed346
Laurens <laurens.aarnoudse@gmail.com>
2018-09-25 13:37:40 +0200

    Wrote very basic API that serves json files in a specific folder to frontend

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const fs = require('fs')
+const path = require('path')
 
 express()
   .use(express.static('static'))
-  .get('/faculty', faculty)
+  .get('/faculty/:id', faculty)
   .listen(8000)
 
-function  faculty(){
-	console.log("someone requested /faculty")
+function  faculty(req,res){
+	let route = req.url
+	let id = req.params.id
+	console.log("someone requested /faculty:id", id)
+	fs.readFile(path.join('static', route + ".json"),'utf8', function(err,data) {
+		if (err) throw err;
+		let result = JSON.parse(data);
+		console.log("Found file: ", result)
+
+	})
 }
