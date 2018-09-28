7e4f40fa24e0ff4de3d457ded47e089e10515268
Laurens <laurens.aarnoudse@gmail.com>
2018-09-28 16:43:41 +0200

    Added support for getting all faculties, all programs and a specific faculty or program. Went with a reusable async-await solution. Hope you don't think this solution is too complex. I tried to think ahead to how it would work when the database is in place. This code already uses some db terminology to help with that.

--- index.js
+++ index.js
@@ -2,19 +2,61 @@ const express = require('express')
 const fs = require('fs')
 const path = require('path')
 
+const db = 'static'
+
 express()
   .use(express.static('static'))
-  .get('/faculty/:id', faculty)
+  .get('/faculty', getFaculties)
+  .get('/faculty/:id', getFaculty)
+  .get('/program', getPrograms)
+  .get('/program/:id', getProgram)
   .listen(8000)
 
-function  faculty(req,res){
+//TODO: make this reusable async just like the getRecord function
+function  getFaculties(req,res){
 	let route = req.url
-	let id = req.params.id
-	console.log("someone requested /faculty:id", id)
-	fs.readFile(path.join('static', route + ".json"),'utf8', function(err,data) {
-		if (err) throw err;
-		let result = JSON.parse(data);
-		console.log("Found file: ", result)
+	let faculties = []
+	fs.readdir(db+route, (err, files) => {
+	  files.forEach(file => {
+	  	let faculty = { id: file}
+	    faculties.push(faculty)
+	  })
+	  res.send(faculties)
+	})	
+}
 
+function  getPrograms(req,res){
+	let route = req.url
+	let programs = []
+	fs.readdir(db+route, (err, files) => {
+	  files.forEach(file => {
+	  	let program = { id: file}
+	    programs.push(program)
+	  })
+	  res.send(programs)
+	})	
+}
+
+async function  getFaculty(req,res){
+	let route = req.url
+	console.log("someone requested /faculty:id", route)
+	result = await getRecord(route)
+	res.send(result)
+}
+
+async function  getProgram(req,res){
+	let route = req.url
+	console.log("someone requested /program:id", route)
+	result = await getRecord(route)
+	res.send(result)
+}
+
+function getRecord(table, encoding = "utf8"){
+	return new Promise( (resolve,reject) => {
+		console.log(db,table)
+		fs.readFile(path.join(db, table+".json"), encoding, function(err,data) {
+		if (err) throw err;
+		resolve (JSON.parse(data));
+		})
 	})
 }
