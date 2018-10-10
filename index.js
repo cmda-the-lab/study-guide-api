b963a445bde0a9642d50c7ef38b8e8098f593023
Laurens <laurens.aarnoudse@gmail.com>
2018-10-10 18:05:56 +0200

    Quickly added a basic post route for courses. If you send it a valid json array all objects will be stored in the Course collection in the mongo db. WARNING:Theres no input validation on the data

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 //Require necessary packages
-const express = require('express')
-const fs = require('fs')
-const path = require('path')
-const mongoose = require('mongoose')
-require('dotenv').config()	//Configure .env so all env vars are loaded
+const express = require("express")
+const fs = require("fs")
+const path = require("path")
+const mongoose = require("mongoose")
+const bodyParser = require('body-parser')
+require("dotenv").config() //Configure .env so all env vars are loaded
 
-const Faculty = require('./models/faculty')
-const Program = require('./models/program')
-const Course = require('./models/course')
-const Competency = require('./models/competency')
-const Indicator = require('./models/indicator')
-const Person = require('./models/person')
-const testSchemas = require('./test-schemas')
+const Faculty = require("./models/faculty")
+const Program = require("./models/program")
+const Course = require("./models/course")
+const Competency = require("./models/competency")
+const Indicator = require("./models/indicator")
+const Person = require("./models/person")
+const testSchemas = require("./test-schemas")
 //Run the next line to test the db schemas
 testSchemas.init()
 
@@ -19,75 +20,106 @@ testSchemas.init()
 const mongooseURL = process.env.MONGO_DB_URL
 
 //Connect to and configure database connection
-mongoose.connect(mongooseURL, { useNewUrlParser: true })
+mongoose.connect(
+  mongooseURL,
+  {
+    useNewUrlParser: true
+  }
+)
 mongoose.Promise = global.Promise
 const db = mongoose.connection
 
 //Set up express and routes
 //TODO: Move routes to proper router
 express()
-  .use(express.static('static'))
-  .get('/faculty', getFaculties)
-  .get('/faculty/:id', getFaculty)
-  .get('/program', getPrograms)
-  .get('/program/:id', getProgram)
-  .get('/person', getPersons)
-  .get('/person/:id', getPerson)
-  .get('/course', getCourses)
-  .get('/course/:id', getCourse)
+  .use(express.static("static"))
+  .use(bodyParser.urlencoded({extended: true}))
+  .get("/faculty", getFaculties)
+  .get("/faculty/:id", getFaculty)
+  .get("/program", getPrograms)
+  .get("/program/:id", getProgram)
+  .get("/person", getPersons)
+  .get("/person/:id", getPerson)
+  .get("/course", getCourses)
+  .get("/course/:id", getCourse)
+  .post("/course", postCourses)
   .listen(8000)
 
 //Some async route functions that get data from the db
-async function  getFaculties(req,res){
-	const result = await Faculty.find()
-	res.send(result)
+async function getFaculties(req, res) {
+  const result = await Faculty.find()
+  res.send(result)
 }
 
-async function  getPrograms(req,res){
-	const result = await Program.find()
-	res.send(result)
+async function getPrograms(req, res) {
+  const result = await Program.find()
+  res.send(result)
 }
 
-async function  getCourses(req,res){
-	const result = await Course.find()
-	res.send(result)
+async function getCourses(req, res) {
+  const result = await Course.find()
+  res.send(result)
 }
 
-async function  getPersons(req,res){
-	const result = await Person.find()
-	res.send(result)
+async function getPersons(req, res) {
+  const result = await Person.find()
+  res.send(result)
 }
 
-async function  getFaculty(req,res){
-	let id = req.params.id
-	console.log("someone requested /faculty:id", id)
-	const result = await Faculty.find({id})
-	res.send(result)
+async function getFaculty(req, res) {
+  let id = req.params.id
+  console.log("someone requested /faculty:id", id)
+  const result = await Faculty.find({
+    id
+  })
+  res.send(result)
 }
 
-async function  getProgram(req,res){
-	let id = req.params.id
-	console.log("someone requested /program:id", id)
-	const result = await Program.find({id})
-	res.send(result)
+async function getProgram(req, res) {
+  let id = req.params.id
+  console.log("someone requested /program:id", id)
+  const result = await Program.find({
+    id
+  })
+  res.send(result)
 }
 
-async function  getCourse(req,res){
-	let id = req.params.id
-	console.log("someone requested /Course:id", id)
-	const result = await Course.find({id})
-	res.send(result)
+async function getCourse(req, res) {
+  let id = req.params.id
+  console.log("someone requested /Course:id", id)
+  const result = await Course.find({
+    id
+  })
+  res.send(result)
 }
 
-async function  getPerson(req,res){
-	let id = req.params.id
-	console.log("someone requested /Person:id", id)
-	const result = await Person.find({id})
-	res.send(result)
+async function getPerson(req, res) {
+  let id = req.params.id
+  console.log("someone requested /Person:id", id)
+  const result = await Person.find({
+    id
+  })
+  res.send(result)
+}
+
+async function postCourses(req, res) {
+  let input = req.body
+  console.log(input.courses)
+  let courses = JSON.parse(input.courses)
+  console.log(typeof courses)
+  Course.insertMany(courses, function(error, docs) {
+      if (error) {
+        console.error('Insert into db failed', error);
+      }
+      else {
+        res.status(200)
+        res.send(docs.length + " docs succesfully inserted into db")
+      }
+    })
 }
 
 //Listeners on the database
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-db.once('open', function() {
+db.on("error", console.error.bind(console, "MongoDB connection error:"))
+db.once("open", function() {
   console.log("Connected to db")
 })
--- test-schemas.js
+++ test-schemas.js
@@ -1,151 +1,176 @@
-const Faculty = require('./models/faculty')
-const Program = require('./models/program')
-const Course = require('./models/course')
-const Competency = require('./models/competency')
-const Indicator = require('./models/indicator')
-const Person = require('./models/person')
+const Faculty = require("./models/faculty")
+const Program = require("./models/program")
+const Course = require("./models/course")
+const Competency = require("./models/competency")
+const Indicator = require("./models/indicator")
+const Person = require("./models/person")
 //This fails, I think the syntax is wrong for the type defs in the schemas
 // Use this: https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/mongoose#Book_model
 const testSchemas = {
-	async init(){
-		//Create a mock database
-		let db = {}
-		//Fill it with a faculty if there isn;t one already
-		//The reason I wait on the faculty function and then await finding it again
-		// is because I want to be able to add extra faculties later and then db.faculty
-		// would wait on Faculty.find()
-		await this.faculty()
-		db.faculty = await Faculty.findOne()
-		//console.log("faculty:", db.faculty)
-		await this.program()
-		db.program = await Program.findOne()
-		await this.competency(db)
-		db.competencies = await Competency.find()
-		await this.indicator(db)
-		db.indicators = await Indicator.find()
-		//Fill it with a person if there isn't one already
-		//console.log(db.faculty)
-		await this.person()
-		db.persons = await Person.find()
-		//console.log(db.persons)
-		await this.course(db)
-		db.courses = await Course.find()
-		//console.log(db.courses)
-	},
-	async faculty(){
-		let faculty = await Faculty.findOne()
-		if (faculty) return faculty
+  async init() {
+    //Create a mock database
+    let db = {}
+    //Fill it with a faculty if there isn;t one already
+    //The reason I wait on the faculty function and then await finding it again
+    // is because I want to be able to add extra faculties later and then db.faculty
+    // would wait on Faculty.find()
+    await this.faculty()
+    db.faculty = await Faculty.findOne()
+    //console.log("faculty:", db.faculty)
+    await this.program()
+    db.program = await Program.findOne()
+    await this.competency(db)
+    db.competencies = await Competency.find()
+    await this.indicator(db)
+    db.indicators = await Indicator.find()
+    //Fill it with a person if there isn't one already
+    //console.log(db.faculty)
+    await this.person()
+    db.persons = await Person.find()
+    //console.log(db.persons)
+    await this.course(db)
+    db.courses = await Course.find()
+    //console.log(db.courses)
+  },
+  async faculty() {
+    let faculty = await Faculty.findOne()
+    if (faculty) return faculty
 
-		const fdmci = new Faculty(
-		{
-			"id": "fdmci",
-  			"name": [
-    			{"language": "nl", "value": "Digitale Media en Creatieve Industrie"},
-    			{"language": "en", "value": "Digital Media and Creative Industry"}
-  			]
-  		})
-  		console.log("New faculty created with id:",fdmci.id)
-  		await fdmci.save()
-		return
-	},
-	async program(){
-		let program = await Program.findOne()
-		if (program) return program
+    const fdmci = new Faculty({
+      id: "fdmci",
+      name: [
+        {
+          language: "nl",
+          value: "Digitale Media en Creatieve Industrie"
+        },
+        {
+          language: "en",
+          value: "Digital Media and Creative Industry"
+        }
+      ]
+    })
+    console.log("New faculty created with id:", fdmci.id)
+    await fdmci.save()
+    return
+  },
+  async program() {
+    let program = await Program.findOne()
+    if (program) return program
 
-		const cmd = new Program(
-		{
-		  "id": "cmd-vt",
-		  "name": [
-		    {"language": "nl", "value" : "Communicatie en Multimedia Design"},
-		    {"language": "en", "value" :"Communcation and Multimedia Design"}
-		  ],
-		  competencies: [],
-		  faculty: db.faculty
-		})
-  		console.log("New program created with id:", cmd.id)
-  		await cmd.save()
-		return
-	},
-	async course(db){
-		let course = await Course.findOne()
-		if (course) return course
-		const designEthics = new Course(
-	  	{
-		id: "257651",
-	  	name: [{
-	      "language": "nl",
-	      "value": "Design Ethics"
-	    }],
-	  	description: "Today, designed objects, services and processes surround us: design has the potential to improve or worsen our lives and the world we live in and designers, by playing a key role in the development of this designed world, are in a position to have a significant influence on people, society, culture and the world. Sometimes this influence may not have moral implications, but on occasions it will. The design ethics module will examine this issue by asking and examining possible answers to the following questions:",
-		year: "2018-2019",
-	  	credits: 3,
-		start: null,
-		end: null,
-	  	languages: ["nl"],
-		coordinators: db.persons[0],
-		coordinatorsSummary: "awesome",
-		teachers: db.persons[0],
-		teachersSummary: "awesometeachers",
-		competencies: db.competencies,
-		competenciesSummary: "research",
-		indicators: db.indicators[0],
-		objectivesSummary: "Objective! Over ruled!",
-		program: db.program,
-		faculty: db.faculty
-		})
-		//console.log(designEthics.coordinators)
-		await designEthics.save(function (err, course) {
-			if (err) return console.error(err)
-			return course
-		})
-		console.log("New Course created with desc:",designEthics.description)
-		return
-	},
-	async competency(db){
-		let competency = await Competency.findOne()
-		if (competency) return competency
+    const cmd = new Program({
+      id: "cmd-vt",
+      name: [
+        {
+          language: "nl",
+          value: "Communicatie en Multimedia Design"
+        },
+        {
+          language: "en",
+          value: "Communcation and Multimedia Design"
+        }
+      ],
+      competencies: [],
+      faculty: db.faculty
+    })
+    console.log("New program created with id:", cmd.id)
+    await cmd.save()
+    return
+  },
+  async course(db) {
+    let course = await Course.findOne()
+    if (course) return course
+    const designEthics = new Course({
+      id: "257651",
+      name: [
+        {
+          language: "nl",
+          value: "Design Ethics"
+        }
+      ],
+      description:
+        "Today, designed objects, services and processes surround us: design has the potential to improve or worsen our lives and the world we live in and designers, by playing a key role in the development of this designed world, are in a position to have a significant influence on people, society, culture and the world. Sometimes this influence may not have moral implications, but on occasions it will. The design ethics module will examine this issue by asking and examining possible answers to the following questions:",
+      year: "2018-2019",
+      credits: 3,
+      start: null,
+      end: null,
+      languages: ["nl"],
+      coordinators: db.persons[0],
+      coordinatorsSummary: "awesome",
+      teachers: db.persons[0],
+      teachersSummary: "awesometeachers",
+      competencies: db.competencies,
+      competenciesSummary: "research",
+      indicators: db.indicators[0],
+      objectivesSummary: "Objective! Over ruled!",
+      program: db.program,
+      faculty: db.faculty
+    })
+    //console.log(designEthics.coordinators)
+    await designEthics.save(function(err, course) {
+      if (err) return console.error(err)
+      return course
+    })
+    console.log("New Course created with desc:", designEthics.description)
+    return
+  },
+  async competency(db) {
+    let competency = await Competency.findOne()
+    if (competency) return competency
 
-		const evalueren = new Competency(
-		{
-		  "id": "4",
-		  "name": [
-		    {"language": "nl", "value": "Evalueren"},
-		    {"language": "en", "value": "Evaluation"}
-		  ],
-		  "description":"",
-		  "indicators": [],
-		  "programs": db.program  
-		})
-  		console.log("New competency created with id:", evalueren.id)
-  		await evalueren.save()
-		return
-	},
-	async indicator(db){
-		let indicator = await Indicator.findOne()
-		if (indicator) return indicator
-		const fourA = new Indicator(
-		{
-		  "id": "4a",
-		  "name": [
-		    {"language": "nl", "value": "Een CMD’er is kritisch op het eigen werk met als doel dit te verbeteren en zoekt actief naar feedback."},
-		    {"language": "en", "value": "A CMD’er is self-critical to be able to improve their work and actively look for feedback."}
-		  ],
-		  "description": "Een CMD’er is kritisch op het eigen werk met als doel dit te verbeteren en zoekt actief naar feedback.",
-		  competency: db.competencies[0],
-		  program: db.program
-		})
-  		console.log("New indicator created with id:", fourA.id)
-  		await fourA.save()
-		return
-	},
-	async person(){
-	  	let person = await Person.findOne()
-		if (person) return person
+    const evalueren = new Competency({
+      id: "4",
+      name: [
+        {
+          language: "nl",
+          value: "Evalueren"
+        },
+        {
+          language: "en",
+          value: "Evaluation"
+        }
+      ],
+      description: "",
+      indicators: [],
+      programs: db.program
+    })
+    console.log("New competency created with id:", evalueren.id)
+    await evalueren.save()
+    return
+  },
+  async indicator(db) {
+    let indicator = await Indicator.findOne()
+    if (indicator) return indicator
+    const fourA = new Indicator({
+      id: "4a",
+      name: [
+        {
+          language: "nl",
+          value: "Een CMD’er is kritisch op het eigen werk met als doel dit te verbeteren en zoekt actief naar feedback."
+        },
+        {
+          language: "en",
+          value: "A CMD’er is self-critical to be able to improve their work and actively look for feedback."
+        }
+      ],
+      description: "Een CMD’er is kritisch op het eigen werk met als doel dit te verbeteren en zoekt actief naar feedback.",
+      competency: db.competencies[0],
+      program: db.program
+    })
+    console.log("New indicator created with id:", fourA.id)
+    await fourA.save()
+    return
+  },
+  async person() {
+    let person = await Person.findOne()
+    if (person) return person
 
-	  	const laurens = new Person({ id: 'aarnl', name: 'Laurens A' })
-		await laurens.save()
-		console.log("New Person created with name:",laurens.name)
-		return
-	}
+    const laurens = new Person({
+      id: "aarnl",
+      name: "Laurens A"
+    })
+    await laurens.save()
+    console.log("New Person created with name:", laurens.name)
+    return
+  }
 }
-module.exports = testSchemas;
+module.exports = testSchemas
