8ddcb8f50ce9253cc284493d2e925d5f6d028a2b
Laurens <laurens.aarnoudse@gmail.com>
2018-10-11 16:19:00 +0200

    Added postroutes and wrote some code to check if you can find documents in collection B by their referenced ObjectIds in collection A. Turns out you can, whoop whoop! One way to improve it would be to use Mongoose's populate but I'll look into that later. Now it's time for a run

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //Require necessary packages
 const express = require("express")
-const fs = require("fs")
+const { promisify } = require("util")
 const path = require("path")
 const mongoose = require("mongoose")
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser")
 require("dotenv").config() //Configure .env so all env vars are loaded
 
 const Faculty = require("./models/faculty")
@@ -31,18 +31,23 @@ const db = mongoose.connection
 
 //Set up express and routes
 //TODO: Move routes to proper router
-express()
+const app = express()
+app
   .use(express.static("static"))
-  .use(bodyParser.urlencoded({extended: true}))
+  .use(bodyParser.urlencoded({ extended: true }))
   .get("/faculty", getFaculties)
+  .post("/faculty", postFaculties)
   .get("/faculty/:id", getFaculty)
   .get("/program", getPrograms)
+  .post("/program", postPrograms)
   .get("/program/:id", getProgram)
   .get("/person", getPersons)
+  .post("/person", postPersons)
   .get("/person/:id", getPerson)
   .get("/course", getCourses)
-  .get("/course/:id", getCourse)
   .post("/course", postCourses)
+  .get("/course/:id", getCourse)
+  .get("/course/:id/teachers", getCourseTeachers)
   .listen(8000)
 
 //Some async route functions that get data from the db
@@ -51,9 +56,37 @@ async function getFaculties(req, res) {
   res.send(result)
 }
 
+function postFaculties(req, res) {
+  let docs = JSON.parse(req.body.faculties)
+  Faculty.insertMany(docs, function(error, docs) {
+    if (error) {
+      console.error("Insert into db failed", error)
+      res.status(406)
+      res.send(error)
+    } else {
+      res.status(200)
+      res.send(docs.length + " docs succesfully inserted into db")
+    }
+  })
+}
+
 async function getPrograms(req, res) {
   const result = await Program.find()
   res.send(result)
+}
+
+function postPrograms(req, res) {
+  let docs = JSON.parse(req.body.programs)
+  Program.insertMany(docs, function(error, docs) {
+    if (error) {
+      console.error("Insert into db failed", error)
+      res.status(406)
+      res.send(error)
+    } else {
+      res.status(200)
+      res.send(docs.length + " docs succesfully inserted into db")
+    }
+  })
 }
 
 async function getCourses(req, res) {
@@ -61,9 +94,37 @@ async function getCourses(req, res) {
   res.send(result)
 }
 
+function postCourses(req, res) {
+  let docs = JSON.parse(req.body.courses)
+  Course.insertMany(docs, function(error, docs) {
+    if (error) {
+      console.error("Insert into db failed", error)
+      res.status(406)
+      res.send(error)
+    } else {
+      res.status(200)
+      res.send(docs.length + " docs succesfully inserted into db")
+    }
+  })
+}
+
 async function getPersons(req, res) {
   const result = await Person.find()
   res.send(result)
+}
+
+function postPersons(req, res) {
+  let docs = JSON.parse(req.body.persons)
+  Person.insertMany(docs, function(error, docs) {
+    if (error) {
+      console.error("Insert into db failed", error)
+      res.status(406)
+      res.send(error)
+    } else {
+      res.status(200)
+      res.send(docs.length + " docs succesfully inserted into db")
+    }
+  })
 }
 
 async function getFaculty(req, res) {
@@ -102,22 +163,17 @@ async function getPerson(req, res) {
   res.send(result)
 }
 
-async function postCourses(req, res) {
-  let input = req.body
-  console.log(input.courses)
-  let courses = JSON.parse(input.courses)
-  console.log(typeof courses)
-  Course.insertMany(courses, function(error, docs) {
-      if (error) {
-        console.error('Insert into db failed', error)
-        res.status(406)
-        res.send(error)
-      }
-      else {
-        res.status(200)
-        res.send(docs.length + " docs succesfully inserted into db")
-      }
-    })
+async function getCourseTeachers(req, res) {
+  let id = req.params.id
+  console.log("someone requested /Course:id/teachers", id)
+  const course = await Course.findOne({
+    id
+  })
+  console.log("teachers for this course: ", course.teachers)
+  const teacherIds = course.teachers.map(teacher => mongoose.Types.ObjectId(teacher))
+  console.log(teacherIds)
+  const result = await Person.find({_id: { $in: teacherIds} })
+  res.send(result)
 }
 
 //Listeners on the database
