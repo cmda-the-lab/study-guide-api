1e429e976431b1a58d876b8ebfeedca113041851
Laurens <laurens.aarnoudse@gmail.com>
2018-11-22 21:19:09 +0100

    Added support for clusters. Course post now finds the relevant cluster(s) for that course and updates each cluster(s)' courses field to include the course being posted. This should also work for multiple courses being posted at once but that is untested. New cluster collection included

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ mongoose.set("debug", Boolean(process.env.DEBUG))
 
 const Faculty = require("./models/faculty")
 const Program = require("./models/program")
+const Cluster = require("./models/cluster")
 const Course = require("./models/course")
 const Competency = require("./models/competency")
 const Indicator = require("./models/indicator")
@@ -51,6 +52,8 @@ app
   .get("/person", getPersons)
   // .post("/person", postPersons)
   // .get("/person/:id", getPerson)
+  .get("/cluster", getClusters)
+  .post("/cluster", postClusters)
   .get("/course", getCourses)
   .post("/course", postCourses)
   .get("/course/:id", getCourse)
@@ -102,6 +105,27 @@ function postPrograms(req, res) {
   })
 }
 
+async function getClusters(req, res) {
+  const result = await Cluster.find()
+  res.send(result)
+}
+
+async function postClusters(req, res) {
+  //Convert courses to array if only one course was sent.
+  let clusters = Array.isArray(req.body) ? req.body : [req.body] //TODO: Move this to middleware
+  debug("incoming Clusters:", clusters)
+  Cluster.insertMany(clusters, function(error, docs) {
+    if (error) {
+      console.error("Insert into db failed", error)
+      res.status(406)
+      res.send(error)
+    } else {
+      res.status(200)
+      res.send(docs.length + " docs succesfully inserted into db")
+    }
+  })
+}
+
 async function getCourses(req, res) {
   const result = await Course.find()
   res.send(result)
@@ -110,24 +134,26 @@ async function getCourses(req, res) {
 //WARNING: Because of the additional indicator lookup, this post route will probably be very slow!
 async function postCourses(req, res) {
   //Convert courses to array if only one course was sent.
-  let courses = Array.isArray(req.body) ? req.body : [req.body] //TODO: Move this to middleware
-  debug("incoming courses:", courses)
+  let newCourses = Array.isArray(req.body) ? req.body : [req.body] //TODO: Move this to middleware
+  debug("incoming courses:", newCourses)
 
-  //For each course in the post, find all it's indicators' competencies then store the relevant competencies
-  // In the relevant course
-  // The code got a little bit complicated because I needed to use async within a map function
-  // await Promise.all(courses.map(async course =>{
-  //   let indis = await Indicator.find({ _id: { $in: course.indicators } })
-  //   debug("indis found:", indis)
-  //   let compes = indis.map(indi => String(indi.competency))
-  //   debug("Compes before unique check:", compes)
-  //   compes = Array.from(new Set(compes)) //Create an array of unique competency ids
-  //   debug("compes unique:", compes)
-  //   course.competencies = compes
-  // }))
-  // debug("Courses are now:", courses)
+  const monCourses = newCourses.map(course => {
+    const newCourse = new Course(course)
+    debug(course.cluster)
+    course.cluster.forEach(clusterId => {
+      Cluster.update(
+        { _id:clusterId}, 
+        { $push: { courses: newCourse._id } },
+        function(error,docs){
+          if(error) {
+            console.log(error)
+          }
+      })
+    })
+    return newCourse
+  })
   
-  Course.insertMany(courses, function(error, docs) {
+  Course.insertMany(monCourses, function(error, docs) {
     if (error) {
       console.error("Insert into db failed", error)
       res.status(406)
@@ -209,6 +235,15 @@ async function getProgram(req, res) {
   let id = req.params.id
   console.log("someone requested /program:id", id)
   const result = await Program.find({
+    id
+  })
+  res.send(result)
+}
+
+async function getCluster(req, res) {
+  let id = req.params.id
+  console.log("someone requested /Cluster:id", id)
+  const result = await Cluster.find({
     id
   })
   res.send(result)
